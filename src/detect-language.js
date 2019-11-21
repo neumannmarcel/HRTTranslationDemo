ad4374e82c96247cbbafcb909f8d7763d7999e25
marcelneumann <marcel.neumann@student.reutlingen-university.de>
2019-11-21 10:11:40 +0100

    First commit

--- src/detect-language.js
+++ src/detect-language.js
@@ -37,7 +37,28 @@ function main(params) {
 
     try {
       
-      // *******TODO**********
+const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
+
+const languageTranslator = new LanguageTranslatorV3({
+  version: '2018-05-01',
+  authenticator: new IamAuthenticator({
+    apikey: 'rboxmj20W_OgkwHl7HPqEaDWnEdsvR5MDeBdl-qPTdhd',
+  }),
+  url: 'https://gateway-fra.watsonplatform.net/language-translator/api',
+});
+
+const identifyParams = {
+  text: 'Language translator translates text from one language to another'
+};
+
+languageTranslator.identify(identifyParams)
+  .then(identifiedLanguages => {
+    console.log(JSON.stringify(identifiedLanguages, null, 2));
+  })
+  .catch(err => {
+    console.log('error:', err);
+  });
       // - Call the language identification API of the translation service
       // see: https://cloud.ibm.com/apidocs/language-translator?code=node#identify-language
       // - if successful, resolve exactly like shown below with the
@@ -51,7 +72,7 @@ function main(params) {
         statusCode: 200,
         body: {
           text: params.text, 
-          language: "<Best Language>",
+          language: "en",
           confidence: 0.5,
         },
         headers: { 'Content-Type': 'application/json' }
